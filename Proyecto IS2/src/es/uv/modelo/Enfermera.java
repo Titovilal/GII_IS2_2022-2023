cf24e50bfb3a783263785a6ec02cebad9e1cfd00
Miguel <Miguel@LAPTOP-N43VK9GB>
2022-10-26 11:52:52 +0200

    Función altaPaciente implementada en el modelo.

--- Proyecto IS2/src/es/uv/modelo/Enfermera.java
+++ Proyecto IS2/src/es/uv/modelo/Enfermera.java
@@ -3,5 +3,9 @@ package es.uv.modelo;
 public class Enfermera {
 
     private String nombre, usuario, contraseña;
-
+    public void altaPaciente(String nombrePaciente, String apellidosPaciente, String dniPaciente)
+    {
+        //base de datos = new Paciente(nombrePaciente, apellidosPaciente, dniPaciente);
+        Paciente p = new Paciente(nombrePaciente, apellidosPaciente, dniPaciente);
+    }
 }
--- Proyecto IS2/src/es/uv/modelo/Paciente.java
+++ Proyecto IS2/src/es/uv/modelo/Paciente.java
@@ -3,5 +3,10 @@ package es.uv.modelo;
 public class Paciente {
 
     private String nombre, apellidos, dni;
-
+    public Paciente( String nombre, String apellidos, String dni)
+    {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.dni = dni;
+    }
 }
