fdadb2e612d66c7b8b6f6217fac80457b10f5a52
Miguel <Miguel@LAPTOP-N43VK9GB>
2022-11-26 19:35:17 +0100

    Creada clase Enfermera. Falta unir con BD.

--- Proyecto IS2/src/es/uv/modelo/Trabajador.java
+++ Proyecto IS2/src/es/uv/modelo/Trabajador.java
@@ -16,32 +16,37 @@ public class Trabajador {
     private int idTrabajador;
     private String usuario, contra;
     private Boolean esMedico;
+    private Enfermera enfermera;
     
     private List<Paciente> aux; //Lista auxiliar hasta que se pueda pasar información a la base de datos
     
     public Trabajador(){
     }
 
-    public Trabajador(int idTrabajador, String medico, String contrasenya, Boolean esMedico) {
+    public Trabajador(int idTrabajador, String usuario, String contrasenya, Boolean esMedico) {
         this.idTrabajador = idTrabajador;
-        this.usuario = medico;
+        this.usuario = usuario;
         this.contra = contrasenya;
         this.esMedico = esMedico;
+        if(!esMedico)//Enfermera
+            enfermera = new Enfermera();
     }
     
     public void crearPaciente(int idPaciente, int habitacion, String dni, String apellidos, String sintomas){
-        aux.add(new Paciente( idPaciente, habitacion, dni, apellidos, sintomas));
+        if(!esMedico)//Enfermera
+            enfermera.crearPaciente(idPaciente, habitacion, dni, apellidos, sintomas);
     }
     
     public List<Medicamento> comprobarBotiquin(int num,AccesoBD bd){
-        
-        List<Medicamento> medicamentos = bd.obtenerMedicamentosBD();
-        Iterator<Medicamento> it = medicamentos.listIterator();
-        while(it.hasNext()){
-            if(it.next().getUnidades()>=num)
-                medicamentos.remove(it);
-        }
-        return medicamentos;        
+        if(!esMedico)//Enfermera
+            return enfermera.comprobarBotiquin( num, bd);
+        return null;
+    }
+    
+    public List<Medicamento> medicamentosDiarios(Paciente p, AccesoBD bd){
+        if(!esMedico)//Enfermera
+            return enfermera.medicamentosDiarios(p, bd);
+        return null;
     }
 
     public int getIdTrabajador() {
@@ -56,8 +61,8 @@ public class Trabajador {
         return usuario;
     }
 
-    public void setUsuario(String medico) {
-        this.usuario = medico;
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
     }
 
     public String getContrasenya() {
