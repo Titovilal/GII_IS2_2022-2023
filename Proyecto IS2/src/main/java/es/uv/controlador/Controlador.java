0227fe1bc71638be3d44ffa62ea8aa4e354d93a3
Titovilal <scastellsjuan@gmail.com>
2022-11-30 12:29:45 +0100

    Transición a Maven

--- Proyecto IS2/src/main/java/es/uv/controlador/Controlador.java
+++ Proyecto IS2/src/main/java/es/uv/controlador/Controlador.java
@@ -8,76 +8,68 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class Controlador 
-{
+public class Controlador {
+
     private Login vista;
-    public Controlador(Login vista)
-    {
+
+    public Controlador(Login vista) {
         this.vista = vista;
         vista.addWindowListener(new ControladorWindowListener());
         vista.setActionListener(new ControladorActionListener());
     }
-    
-    class ControladorWindowListener extends WindowAdapter
-    {
+
+    class ControladorWindowListener extends WindowAdapter {
+
         @Override
-        public void windowClosing(WindowEvent e)
-        {
+        public void windowClosing(WindowEvent e) {
             System.exit(0);
         }
     }
-    
-    class ControladorActionListener implements ActionListener
-    {
+
+    class ControladorActionListener implements ActionListener {
+
         @Override
-        public void actionPerformed(ActionEvent ae) 
-        {
+        public void actionPerformed(ActionEvent ae) {
             String command = ae.getActionCommand();
-            switch (command)
-            {
-                case "aceptarMedicoMedicamento":
-                {
+            switch (command) {
+                case "aceptarMedicoMedicamento": {
                     System.out.println("Todo en orden");
                     break;
                 }
-                  
-                case "volverMedicoMedicamento":
-                {
+
+                case "volverMedicoMedicamento": {
                     break;
                 }
-                    
-                case "restablecerMedicoMedicamento":
-                {
+
+                case "restablecerMedicoMedicamento": {
                     break;
                 }
-                    
-                case "comprobarBotiquin":
-                {
+
+                case "comprobarBotiquin": {
                     /*
                         vista.get(num);
                         vista.setMed(Trabajador.comprobarBotiquin(num,bd));
                         
-                    */
-                    break;    
+                     */
+                    break;
                 }
                 case "crearPacienteEnfermera":
-                    System.out.println("Paciente creado con los datos:\n" + 
-                    vista.enfermera_getDNI()+"\n"+vista.enfermera_getNombre()+"\n"+
-                    vista.enfermera_getApellidos()+"\n"+vista.enfermera_getHabitacion()+"\n"+
-                    vista.enfermera_getSintomas());
+                    System.out.println("Paciente creado con los datos:\n"
+                            + vista.enfermera_getDNI() + "\n" + vista.enfermera_getNombre() + "\n"
+                            + vista.enfermera_getApellidos() + "\n" + vista.enfermera_getHabitacion() + "\n"
+                            + vista.enfermera_getSintomas());
                     break;
-                default:
-                {
+                default: {
                     break;
                 }
             }
         }
-        
+
     }
-    
+
     /**
      * Clase empotrada medicMedicamento
-     * 
+     *
      * Esta clase implementa a la interfaz ActionListener, y tiene como objetivo
      * leer todas las acciones de los botones de la vista MedicoMedicamento. Se
      * encuentra por ahora sometida a posibles cambios
@@ -114,7 +106,7 @@ public class Controlador
                         vista.get(num);
                         vista.setMed(Trabajador.comprobarBotiquin(num,bd));
                         
-                    *//*
+     *//*
                     break;    
                 }
                 
