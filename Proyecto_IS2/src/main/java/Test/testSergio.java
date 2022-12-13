99cfc312af2daaf67ae768e877765e0468d0f5f5
vipaser <vipaser@alumni.uv.es>
2022-12-13 18:46:20 +0100

    TestSergio añadido

---
+++ Proyecto_IS2/src/main/java/Test/testSergio.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Test;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author scast
+ */
+public class testSergio {
+    @Test
+    public void testVisitarMedico (){
+        Paciente tEjemploBien = new Trabajador(1, "medico", "medico", true);
+        Trabajador tEjemploMal = new Trabajador();
+        Trabajador resBien;
+        Trabajador resMal;
+        
+        
+        resBien = AccesoBD.loginTrabajador("medico", "medico");
+        resMal = AccesoBD.loginTrabajador("medico", "medique");
+        
+        assertEquals(resBien, tEjemploBien);
+        assertEquals(resMal, tEjemploMal);
+        fail("el test falla");
+    }
+}
